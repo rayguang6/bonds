e6d1c0d4fe5794886da84ac793fab04e943d7ce7
lemonmel <76807277+lemonmel@users.noreply.github.com>
2022-06-15 02:19:05 +0800

    Update resident_controller.php
    import code from chinfang

--- includes/controllers/resident_controller.php
+++ includes/controllers/resident_controller.php
@@ -1,5 +1,7 @@
 <?php
 include("../../config/config.php");
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 // DELETE 
 if(isset($_POST['delete_resident']))
@@ -7,9 +9,10 @@ if(isset($_POST['delete_resident']))
     $resident_id = mysqli_real_escape_string($con, $_POST['resident_id']);
 
     $query = "DELETE FROM resident WHERE ic='$resident_id'";
+    $query2 = "UPDATE unit SET owner_ic = '' WHERE owner_ic = '$resident_id'";
+    $query_run2 = mysqli_query($con, $query2);
     $query_run = mysqli_query($con, $query);
-
-    if($query_run)
+    if($query_run && $query_run2)
     {
         $res = [
             'status' => 200,
@@ -59,46 +62,4 @@ if(isset($_GET['resident_id']))
         return;
     }
 }
-
-// Update Student
-if(isset($_POST['update_student']))
-{
-    $student_id = mysqli_real_escape_string($con, $_POST['editResident-ic']);
-    $name = mysqli_real_escape_string($con, $_POST['editResident-name']);
-    $contact = mysqli_real_escape_string($con, $_POST['editResident-contact']);
-
-    if($name == NULL )
-    {
-        $res = [
-            'status' => 422,
-            'message' => 'All fields are mandatory'
-        ];
-        echo json_encode($res);
-        return;
-    }
-
-    $query = "UPDATE resident SET name='$name', contact='$contact' 
-                WHERE ic='$student_id'";
-    $query_run = mysqli_query($con, $query);
-
-    if($query_run)
-    {
-        $res = [
-            'status' => 200,
-            'message' => 'Student Updated Successfully'
-        ];
-        echo json_encode($res);
-        return;
-    }
-    else
-    {
-        $res = [
-            'status' => 500,
-            'message' => 'Student Not Updated'
-        ];
-        echo json_encode($res);
-        return;
-    }
-}
-
 ?>
