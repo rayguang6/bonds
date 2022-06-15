be46548cd08b7094fa22b86f8270a728e987447f
lemonmel <76807277+lemonmel@users.noreply.github.com>
2022-06-15 17:24:37 +0800

    Update admin-dashboard.php
    update display announcement, residents and visitors number
    (ACTIVE CASE NOT YET)

--- admin-dashboard.php
+++ admin-dashboard.php
@@ -2,14 +2,18 @@
 $pageTitle = "Dashboard";
 include 'includes/admin-header.php';
 ?>
-
-
                  <div class="row p-0">
                         <div class="col-md-6 col-lg-4 mt-4 mt-lg-0">
                             <div class="mycontainer">
                                 <div class="d-flex align-items-center py-auto">
                                     <i class="bi bi-people-fill fs-2 text-primary"></i>
-                                    <h2 class="ms-2">166</h2>
+                                    <h2 class="ms-2">
+                                        <?php
+                                        $data_query = mysqli_query($con, "SELECT * FROM resident");
+                                        $resident_num = mysqli_num_rows($data_query);
+                                        ?>
+                                        <?= $resident_num ?>
+                                    </h2>
                                 </div>
                                 <h5>Total Residents</h5>
                                 <a href="admin-manageuser.php" class="btn btn-primary">Manage Resident</a>
@@ -20,7 +24,14 @@ include 'includes/admin-header.php';
                             <div class="mycontainer">
                                 <div class="d-flex align-items-center py-auto">
                                     <i class="bi bi-shield-fill-plus fs-2 text-primary"></i>
-                                    <h2 class="ms-2">13</h2>
+                                    <h2 class="ms-2">
+                                        <?php
+                                        //how to find ah????
+                                        $query = mysqli_query($con, "SELECT active_cases FROM covidreport WHERE report_id in (SELECT MAX(report_id) FROM covidreport)");
+                                        $covid_num = mysqli_num_rows($data_query);
+                                        ?>
+                                        <?= $covid_num ?>
+                                    </h2>
                                 </div>
                                 <h5>Active Cases</h5>
                                 <a href="admin-managecovid.php" class="btn btn-primary">Manage Covid</a>
@@ -31,9 +42,16 @@ include 'includes/admin-header.php';
                             <div class="mycontainer">
                                 <div class="d-flex align-items-center py-auto">
                                     <i class="bi bi-person-badge-fill fs-2 text-primary"></i>
-                                    <h2 class="ms-2">5</h2>
+                                    <h2 class="ms-2">
+                                        <?php
+                                            $today = date('Y-m-d H:i:s', mktime(0,0,0));
+                                            $data_query = mysqli_query($con, "SELECT * FROM visitorpass WHERE start_time >'$today'");
+                                            $visitor_num = mysqli_num_rows($data_query);
+                                        ?>
+                                        <?= $visitor_num ?>
+                                    </h2>
                                 </div>
-                                <h5>Today's Visitor</h5>
+                                <h5>Upcoming Visitors</h5>
                                 <a href="admin-manageuser.php#VisitorTable" class="btn btn-primary">Manage Visitor</a>
                             </div>
                         </div>
@@ -46,40 +64,86 @@ include 'includes/admin-header.php';
                                 <!-- announcement submit form -->
                                 <div class="mycontainer">
                                     <h2>Post A New Announcement</h2>
-                                    <form id="" method="POST" action="admin-dashboard.php">
+                                    <form id="announcement" method="POST" action="admin-dashboard.php">
                                         <div class="input-group mb-3">
-                                            <input type="text" class="form-control" placeholder="Title" name="announcement-title">
+                                            <input type="text" class="form-control" placeholder="Title" name="announcement-title" required>
                                         </div>
                                         <div class="input-group mb-3">
-                                            <textarea class="form-control" placeholder="Your Announcement Content Here" name="announcement-content"></textarea>
+                                            <textarea class="form-control" placeholder="Your Announcement Content Here" name="announcement-content" required></textarea>
                                         </div>
-                                        <button type="submit" class="btn btn-primary" name="announcement_btn" >Post</button>
+                                        <button type="submit" class="btn btn-primary" name="announcement_btn">Post</button>
                                     </form>
                                 </div>
 
                                 <?php
+                                        function echoToast($message){
+                                            echo '<div aria-live="polite" aria-atomic="true" class="position-relative" style="z-index: 100;">
+                                                <div class="toast-container position-fixed bottom-0 end-0 m-3">
+                                                    <div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="6000">
+                                                        <div class="toast-header">
+                                                            <span class="bg-primary px-2 rounded">&nbsp;</span>
+                                                            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+                                                        </div>
+                                                        <div class="toast-body">
+                                                            '.$message.'
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <script> $(document).ready(function() {
+                                                $(".toast").toast("show");
+                                            });</script>';
+                                            $_POST = array();
+                                        }
                                         // Method To Submit Announcement
                                         if(isset($_POST['announcement_btn'])){
                                             $title = $_POST['announcement-title'];
                                             $content = $_POST['announcement-content'];
                                             $time = date("Y-m-d h:i:s");
 
-                                            if($title!=""){
-                                                $query = mysqli_query($con, "INSERT INTO announcement VALUES ('','$title','$content','$time')");
+                                            $query_run = mysqli_query($con, "INSERT INTO announcement (title, description, date_time) VALUES ('$title','$content','$time')");
+
+                                            if($query_run) { 
+                                                echoToast("New announcement posted.");
+                                            }else{ 
+                                                echoToast("Post failed. Please try again.");
                                             }
                                         }
 
-                                    ?>
+                                ?>
     
                                 <!-- Announcement Container -->
-                                <div id="announcement_container"></div>
+                                <div id="announcement_container">
+                                <?php
+                                    $data_query = mysqli_query($con, "SELECT * FROM announcement ORDER BY date_time DESC");
+
+                                    if(mysqli_num_rows($data_query) > 0)
+                                    {
+                                        foreach($data_query as $announcement)
+                                        {
+                                            $formattedDate = $announcement['date_time'];
+                                ?>
+                                        <div class="card mt-4 mycontainer">
+                                            <div class="card-body">
+                                                <h5><?=$announcement['title']?></h5>
+                                                <span class="text-muted ms-auto"><?=$formattedDate?></span>
+                                                <p class="card-text mt-3" style="white-space: pre-line"><?=$announcement['description']?></p>
+                                            </div>
+                                        </div>  
+                                <?php
+                                        }
+                                    }
+                                ?>
+                                </div>
                             </div>
                         </div>
                         <!-- Modal End -->
 
                         <!-- (Included In This file Only)Manage Covid JS -->
-                        <script src="assets/js/announcement.js"></script>
+                        <!-- <script src="assets/js/announcement.js"></script> -->
 
 <?php include 'includes/admin-footer.php';?>
+
+                        
 
                         
